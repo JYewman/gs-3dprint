954eef89ef8729c39d3ff9c0f5faf464d09a2acc
JYewman <joshua.yewman@outlook.com>
2023-08-02 09:26:05 +0100

    add emoji support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,15 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    '...',
+    'sphinxemoji.sphinxemoji',
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
